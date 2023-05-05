160929e328da287c488948bcce473991bcf551b2
= <=>
2023-05-04 21:26:24 -0700

    tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = taskText;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => {
+        listItem.remove();
+      });
+
+      listItem.appendChild(deleteButton);
+      taskList.appendChild(listItem);
+    }
+
+    taskInput.value = "";
+  });
 });
